398a8f523e85d4ef3db89efd0cf3a3937b00bf21
MehediHasan06 <mehedihasanmasud06@gmail.com>
2022-01-17 17:04:46 +0600

    new snippet

--- 9.apiCall.js
+++ 9.apiCall.js
@@ -3,11 +3,10 @@
   XMLHttpRequest.prototype.send = function() { 
       this.addEventListener('load', function(e) {
           if(this.responseURL.match(/https:\/\/victoriaplum\.com\/(browse|search\/filter)/i)) {
-              window.dispatchEvent(new CustomEvent('WTO:product-ajax', {
-                  detail: e
-              }));
+              
           }
       });
       return send.apply(this, arguments);
   };
 })();
+
