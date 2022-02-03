a468f2456f0a3c6147698f637cf54327c6dcabd7
Mehedi Hasan <mehedihasanmasud06@gmail.com>
2022-02-03 21:14:39 +0600

    API response snippet

--- 9.apiCall.js
+++ 9.apiCall.js
@@ -1,12 +1,12 @@
-(function() {
+(function () {
   var send = XMLHttpRequest.prototype.send;
-  XMLHttpRequest.prototype.send = function() { 
-      this.addEventListener('load', function(e) {
-          if(this.responseURL.match(/https:\/\/victoriaplum\.com\/(browse|search\/filter)/i)) {
-              
-          }
-      });
-      return send.apply(this, arguments);
+  XMLHttpRequest.prototype.send = function () {
+    this.addEventListener('load', function (e) {
+      if (this.responseURL.match(/https:\/\/victoriaplum\.com\/(browse|search\/filter)/i)) {
+        console.log(JSON.parse(this.responseText));
+      }
+    });
+    return send.apply(this, arguments);
   };
 })();
 
